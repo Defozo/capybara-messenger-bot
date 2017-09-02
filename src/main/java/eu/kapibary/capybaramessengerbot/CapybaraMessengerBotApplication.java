cab54eab063bfa256a3346572e68085175f4654e
Michał Kiełtyka <defozo@gmail.com>
2017-09-02 12:08:53 +0200

    Initial commit

---
+++ src/main/java/eu/kapibary/capybaramessengerbot/CapybaraMessengerBotApplication.java
@@ -0,0 +1,12 @@
+package eu.kapibary.capybaramessengerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapybaraMessengerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapybaraMessengerBotApplication.class, args);
+	}
+}
